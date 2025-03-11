6cc81247840005d7441da91659dfd94df666d03f
RICCARDO ANGUILLETTI s338927 <s338927@studenti.polito.it>
2025-03-11 08:32:35 +0100

    week03

--- week03/exercise.mjs
+++ week03/exercise.mjs
@@ -1,4 +1,11 @@
 import dayjs from "dayjs";
+import sqlite from "sqlite3";
+
+const db = new sqlite.Database("questions.sqlite", (err) => {
+  if(err){
+    throw err;
+  }
+});
 
 // Oggetto rappresentante le domande (con le sue proprietà e metodi)
 function Question(id, text, email, userId, date) {
@@ -9,13 +16,49 @@ function Question(id, text, email, userId, date) {
   this.date = dayjs(date);
 
   // metodo per prendere tutte le risposte della Question instanziata (3)
-  this.getAnswers = () => {}
+  this.getAnswers = () => {
+    return new Promise((resolve,reject)=>{
+      const sql= "SELECT answer.*, user.email FROM answer JOIN user ON answer.authorId = user.id WHERE answer.id= ?";
+      db.all(sql,[this.id],(err,row)=>{
+        if(err){
+          reject(err);
+        }else{
+          const answers = row.map(row=>new Answer(ans.id, ans.text, ans.email, ans.authorId, ans.date, ans.score));
+          resolve(answers);
+        }
+      });
+
+    });
+  }
 
   // metodo per aggiungere una nuova risposta di un autore esistente alla Question instanziata (4)
-  this.addAnswer = (answer) => {}
+  this.addAnswer = (answer) => {
+    return new Promise((resolve, reject) => {
+      const sql = "INSERT INTO answer (id, text, authorId, date) VALUES (?, ?, ?, ?)";
+      db.run(sql, [answer.id, answer.text, answer.authorId, answer.date.toISOString()], function(err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      });
+    });
+  }
 
   // metodo per votare una risposta esistente, con up = +1 e down = -1 (5)
-  this.voteAnswer = (answerId, value) => {}
+  this.voteAnswer = (answerId, value) => {
+    return new Promise((resolve, reject) => {
+      const sql = "UPDATE answer SET score = score + ? WHERE id = ?"; // i punti integgroga il valore di score con il valore di value
+      const delta = value === "up" ? 1 : -1; //if su una riga
+      db.run(sql, [delta, answerId], function(err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
 }
 
 // Oggetto rappresentante le risposte (con le sue proprietà)
@@ -31,10 +74,11 @@ function Answer(id, text, email, userId, date, score = 0) {
 // Oggetto rappresentante la lista di domande (con i suoi metodi)
 function QuestionList() {
 
+
   // metodo per recuperare una singola Question dato il suo ID (1)
   this.getQuestion = (id) => {
     return new Promise((resolve,reject)=>{
-      const sql= "SELECT question.*, user.email FROM question JOIN user ON question.authorId = user.id WHERE id= ?";
+      const sql= "SELECT question.*, user.email FROM question JOIN user ON question.authorId = user.id WHERE question.id= ?";
       db.get(sql,[id],(err,row)=>{
         if(err){
           reject(err);
@@ -43,15 +87,31 @@ function QuestionList() {
         }
       });
 
-    })
+    });
   }
 
   // metodo per aggiungere una nuova Question di un autore esistente (2)
-  this.addQuestion = (question) => {}
+  this.addQuestion = (question) => {
+    return new Promise((resolve, reject) => {
+      const sql = "INSERT INTO question (text, authorId, date) VALUES (?, ?, ?)";
+      db.run(sql, [question.text, question.userId, question.date.toISOString()], function(err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      });
+    });
+  }
 }
 
 // funzione per il test
-function main() {
+async function main() {
+  const questionList = new QuestionList();
+  const question = await questionList.getQuestion(1);
+  console.log(await questionList.getQuestion(1));
+
+  console.log(await question.getAnswers());
   return;
 }
 
